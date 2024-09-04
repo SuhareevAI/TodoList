e5cc7035a78c65e45ef26ee0d10e10a615ac6e5c
suhareevai <sukhareev@yandex.ru>
2024-09-04 23:29:41 +0300

    Сreate app template, set settings

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = 2233;
+const dist = path.join(__dirname, 'dist');
+const src = path.join(__dirname, 'src');
+const host = 'localhost';
+
+module.exports = (_, args) => {
+  return {
+    entry: './index.tsx',
+    devtool: 'source-map',
+    context: src,
+    devServer: {
+      open: true,
+      port,
+      hot: true,
+      historyApiFallback: true,
+      host,
+    },
+    resolve: {
+      modules: [src, 'node_modules'],
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      alias: {
+        src,
+      },
+    },
+    output: {
+      path: dist,
+      filename: `js/[name].js`,
+      chunkFilename: `js/[name].js`,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts)x?$/,
+          loader: require.resolve('babel-loader'),
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.svg/,
+          type: 'asset/inline',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+      }),
+    ],
+  };
+};
